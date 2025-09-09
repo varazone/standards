b0f788d9ba26bae1a595bfbb3f9e704b80dead7f
Broken Pipe <navigaid@gmail.com>
2025-09-09 08:41:19 +0800

    update extended-vmt

--- extended-vmt/app/src/services/extended_vmt/mod.rs
+++ extended-vmt/app/src/services/extended_vmt/mod.rs
@@ -25,6 +25,7 @@ static mut EXTENDED_STORAGE: Option<ExtendedStorage> = None;
 #[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Encode, Decode, TypeInfo)]
 #[codec(crate = sails_rs::scale_codec)]
 #[scale_info(crate = sails_rs::scale_info)]
+#[event]
 pub enum Event {
     Minted {
         to: ActorId,
@@ -73,16 +74,17 @@ impl ExtendedService {
                 .expect("Extended vmt is not initialized")
         }
     }
-}
 
-#[service(extends = VmtService, events = Event)]
-impl ExtendedService {
     pub fn new() -> Self {
         Self {
             vmt: VmtService::new(),
         }
     }
+}
 
+#[service(extends = VmtService, events = Event)]
+impl ExtendedService {
+    #[export]
     pub fn mint(
         &mut self,
         to: ActorId,
@@ -108,6 +110,7 @@ impl ExtendedService {
         self.emit_event(event).expect("Notification Error");
     }
 
+    #[export]
     pub fn mint_batch(
         &mut self,
         to: ActorId,
@@ -133,6 +136,7 @@ impl ExtendedService {
         self.emit_event(event).expect("Notification Error");
     }
 
+    #[export]
     pub fn burn(&mut self, from: ActorId, id: TokenId, amount: U256) {
         if !self.get().burners.contains(&msg::source()) {
             panic!("Not allowed to burn")
@@ -151,6 +155,7 @@ impl ExtendedService {
         self.emit_event(event).expect("Notification Error");
     }
 
+    #[export]
     pub fn burn_batch(&mut self, from: ActorId, ids: Vec<TokenId>, amounts: Vec<U256>) {
         if !self.get().burners.contains(&msg::source()) {
             panic!("Not allowed to burn")
@@ -169,39 +174,53 @@ impl ExtendedService {
         self.emit_event(event).expect("Notification Error");
     }
 
+    #[export]
     pub fn grant_admin_role(&mut self, to: ActorId) {
         self.ensure_is_admin();
         self.get_mut().admins.insert(to);
     }
+
+    #[export]
     pub fn grant_minter_role(&mut self, to: ActorId) {
         self.ensure_is_admin();
         self.get_mut().minters.insert(to);
     }
+
+    #[export]
     pub fn grant_burner_role(&mut self, to: ActorId) {
         self.ensure_is_admin();
         self.get_mut().burners.insert(to);
     }
 
+    #[export]
     pub fn revoke_admin_role(&mut self, from: ActorId) {
         self.ensure_is_admin();
         self.get_mut().admins.remove(&from);
     }
+
+    #[export]
     pub fn revoke_minter_role(&mut self, from: ActorId) {
         self.ensure_is_admin();
         self.get_mut().minters.remove(&from);
     }
+
+    #[export]
     pub fn revoke_burner_role(&mut self, from: ActorId) {
         self.ensure_is_admin();
         self.get_mut().burners.remove(&from);
     }
+
+    #[export]
     pub fn minters(&self) -> Vec<ActorId> {
         self.get().minters.clone().into_iter().collect()
     }
 
+    #[export]
     pub fn burners(&self) -> Vec<ActorId> {
         self.get().burners.clone().into_iter().collect()
     }
 
+    #[export]
     pub fn admins(&self) -> Vec<ActorId> {
         self.get().admins.clone().into_iter().collect()
     }
@@ -214,8 +233,9 @@ impl ExtendedService {
         };
     }
 }
-impl AsRef<VmtService> for ExtendedService {
-    fn as_ref(&self) -> &VmtService {
-        &self.vmt
+
+impl From<ExtendedService> for VmtService {
+    fn from(exts: ExtendedService) -> Self {
+        exts.vmt
     }
 }
